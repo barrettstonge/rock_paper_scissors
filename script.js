97103381bd03276b6f9e21624e6b1331f033ebde
barrettstonge <238751644+barrettstonge@users.noreply.github.com>
2026-02-08 12:11:58 -0500

    created prompt asking if user woould like to play and responses to continiue the logic

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-console.log('Hello world!')
+console.log('Hello world! Let\'s play a game!')
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+  return Math.floor(Math.random() * max);}
 
 function getComputerChoice(x){
     switch (x) {
@@ -19,6 +18,18 @@ function getComputerChoice(x){
 
 
 console.log(getComputerChoice(getRandomInt(3)))
+
+let sign = window.prompt("Would you like to play rock, paper, scissors? yes/no");
+  if (sign.toLowerCase() === "yes") { sign = prompt("Alright, let\'s play! rock, paper or scissors?(case sensitive)");} 
+  
+    else if (sign === null) {
+    sign = prompt ("OK, maybe next time.");}
+    
+    else if (sign.toLowerCase() === "") {
+    sign = prompt("Don't be shy, enter your call!");}
+    
+    else if (sign === "no")( sign = prompt ("Well that\'s a shame, if you change your mind, hit refresh and we can play."))
+
 
  
 
